b50c02c3ebac4a04953a7a8c2b165ab25c1d532b
VK <93388918+vag4me@users.noreply.github.com>
2022-07-16 21:46:16 +0300

    Create count_YOUR_MONEY.cpp

---
+++ count_YOUR_MONEY.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include <sstream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+	bool choice;
+	int day,month,money;
+	string line;
+	string delimiter = ","; // gia na gnorizei se poio character na kanei split 
+	size_t pos = 0;
+	string token;
+	stringstream ss; 
+	int numb,total = 0;
+	
+	ofstream file("elements.txt",ios::app);
+	
+	if(!file)
+	{
+		cout << "Error" << endl;
+	}
+	
+	while(1)
+	{
+		cout << "press 0 to see the earnings" << endl;
+		cout << "press 1 to give a new date" << endl;
+		cin >> choice;
+		if(choice == 0)
+		{
+			break;
+		}
+		else 
+		{
+			cout << "give the day " << endl;
+			cin >> day;
+			cout << "give the month " << endl;
+			cin >> month;	
+			cout << "give the money" << endl;
+			cin >> money;
+			file << day << "/" << month << "/2022  " << money << "$" << endl;		
+		}	
+    }
+    
+	file.close();
+	
+	ifstream file2("elements.txt");
+	
+	
+	if(!file2)
+	{
+		cout << "error";
+	}
+	
+	while(getline (file2, line))
+	{
+		cout << line << endl;
+		pos = line.find(delimiter);
+		token = line.substr(pos+1, '/0');  
+		stringstream saver(token);
+		saver >> numb;
+		total += numb;
+	}
+	
+	cout << "money in total = " << total;
+
+	
+	
+	return 0;
+}
